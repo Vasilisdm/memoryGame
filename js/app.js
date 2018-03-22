2e2ae68748bc17cdd15549e32a3756276e3409dc
= <=>
2018-03-23 00:07:57 +0200

    removed the unecessary comments and refactored the code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-/*
- * Create a list that holds all of your cards
- */
+// list of the available cards
 let allCards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube',
                 'fa-anchor', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube',
                 'fa-anchor', 'fa-leaf', 'fa-bicycle'];
                 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// necessary variables initialization
+let cardList = document.querySelectorAll('.card');
+let openCardList = [];
+let movesCounter = 1;
+let stars = document.querySelector('.stars');
+let restart = document.querySelector('.restart');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(allCards) {
@@ -27,24 +25,7 @@ function shuffle(allCards) {
     return allCards;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-let cardList = document.querySelectorAll('.card');
-let openCardList = [];
-let movesCounter = 1;
-let stars = document.querySelector('.stars');
-let restart = document.querySelector('.restart');
-
+// game restart
 restart.addEventListener('click', function(){
 
     cardList.forEach(function(card){
@@ -53,6 +34,7 @@ restart.addEventListener('click', function(){
 
     movesCounter = 0;
     counterIncrement(movesCounter++);
+
 });
 
 cardList.forEach(function(card) {
